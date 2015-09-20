25e3f35cefe3084e2105b9495e8d524ecb43d5af
Swelly <Surfswell17@gmail.com>
2015-09-20 13:35:18 -0400

    State is being re-set with new note

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -18,21 +18,33 @@ export default class App extends React.Component {
         },
         {
           id: uuid.v4(),
-          task: 'Do laundry'
+          task: 'Do Stuff'
         }
       ]
     };
+
+    // Binding 'this' so 'this' in setState
+    // is in context of the App state
+    this.addNote = this.addNote.bind(this);
   }
   render() {
     const notes = this.state.notes;
 
     return (
       <div>
+      <button addClass="add-note" onClick={this.addNote}>+</button>
         <Notes items={notes} />
       </div>
     );
   }
-    addNote() {
-    console.log('add note');
+  // State is immutable so we set
+  // the state with a new task in place
+  addNote() {
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New Task'
+      }])
+    });
   }
 }
