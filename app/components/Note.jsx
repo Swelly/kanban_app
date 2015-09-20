e2cf49c33a6822f3aa19f10aa4b3e5d7fd956f10
Swelly <Surfswell17@gmail.com>
2015-09-20 13:52:10 -0400

    starting to implement edit function

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
 
 export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Binding edit functions to Tasks
+    this.finishEdit = this.finishEdit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+    this.edit = this.edit.bind(this);
+    this.renderEdit = this.renderEdit.bind(this);
+    this.renderTask = this.renderTask.bind(this);
+
+    // Default state = false
+    this.state = {
+      editing: false
+    };
+  }
+
   render() {
-    return <div>{this.props.task}</div>;
+    const editing = this.state.editing;
+
+    return (
+      <div>
+        {editing ? this.renderEdit() : this.renderTask()}
+      </div>
+    );
+  }
+
+
+  // Visual state for editing task text
+  renderEdit() {
+    return <input type="text"
+      autofocus={true}
+      defaultValue={this.props}
+      onBlue={this.finishEdit}
+      onKeyPress={this.checkEnter} />
+  }
+  // Pass task text on click
+  renderTask() {
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  }
+
+  // edit function changes state constant to true
+  edit() {
+    this.setState({
+      editing: true
+    });
+  }
+
+  // Check for enter key to finish editing input
+  checkEnter(e) {
+    if(e.key === 'Enter') {
+      this.finishEdit(e)
+    }
+  }
+
+  // Set state editing back to default (false)
+  finishEdit(e) {
+    this.props.onEdit(e.target.value);
+
+    this.setState({
+      editing: false
+    });
   }
 }
